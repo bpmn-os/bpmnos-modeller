b81e8bd6b35281fbf64076627dd3905b3bcc517b
Asvin Goel <goel@telematique.eu>
2022-04-28 20:38:43 +0200

    Switch to webpack

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var CopyPlugin = require('copy-webpack-plugin');
+
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              [ '@babel/plugin-transform-react-jsx', {
+                'importSource': '@bpmn-io/properties-panel/preact',
+                'runtime': 'automatic'
+              } ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },      {
+        test: /\.bpmn$/,
+        use: {
+          loader: 'raw-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    mainFields: [
+      'browser',
+      'module',
+      'main'
+    ],
+    alias: {
+      'react': '@bpmn-io/properties-panel/preact/compat'
+    },
+/*
+    modules: [
+      'node_modules',
+      absoluteBasePath
+    ]
+*/
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/index.html', to: '.' },
+        { from: 'node_modules/bpmn-js/dist/assets', to: 'vendor/bpmn-js/' },
+        { from: 'node_modules/bpmn-js-properties-panel/dist/assets', to: 'vendor/bpmn-js-properties-panel/' },
+        { from: 'node_modules/bpmn-js-token-simulation/assets/css', to: 'vendor/bpmn-js-token-simulation/' },
+        { from: 'src/modules/resource/css', to: 'modules/resource/' },
+      ]
+    })
+  ]
+};
