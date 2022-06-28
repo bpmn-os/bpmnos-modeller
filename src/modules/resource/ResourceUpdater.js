abe6eb7f58d6d3fd1b5c3a0ad2862f8a7296cea9
Asvin Goel <goel@telematique.eu>
2022-06-28 14:07:57 +0200

    Replace ids when copying extension elements

--- src/modules/resource/ResourceUpdater.js
+++ src/modules/resource/ResourceUpdater.js
@@ -2,6 +2,8 @@ import inherits from 'inherits';
 
 import { is } from 'bpmn-js/lib/util/ModelUtil';
 
+import Ids from 'ids';
+
 import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
 
 import subProcessTemplates from './SubProcessTemplates.bpmn';
@@ -61,10 +63,26 @@ function preventResize(evt) {
   evt.context.newBounds = { x: evt.context.shape.x, y: evt.context.shape.y, width: evt.context.shape.width, height: evt.context.shape.height };
 }
 
+
+function replaceIds(obj) {
+  for (var key in obj) {
+    if (obj[key] !== null && typeof obj[key] === "object") {
+      // Recurse into children
+      replaceIds(obj[key]);
+    }
+    else if ( key == "id" && obj[key].includes("_") ) {
+      // Replace id of all elements including an underscore within the id
+      const ids = new Ids([ 32,32,1 ]);
+      obj[key] =  ids.nextPrefixed( obj[key].substring(0, obj[key].lastIndexOf('_') + 1) ); 
+    }
+  }
+}
+
+
 /**
  * A handler responsible for creating children to a resource subprocess when this is created
  */
-export default function ResourceUpdater(eventBus, modeling, elementFactory, elementRegistry, editorActions, contextPad, dragging, directEditing) {
+export default function ResourceUpdater(eventBus, modeling, /*elementFactory,*/ elementRegistry, editorActions, contextPad, dragging, directEditing) {
 
   CommandInterceptor.call(this, eventBus);
 
@@ -80,11 +98,9 @@ export default function ResourceUpdater(eventBus, modeling, elementFactory, elem
     // put into clipboard
     targetClipboard.set(children[businessObject.type]);
 
-//    const parent = targetElementRegistry.get(businessObject.id + '_plane');
-// console.log(parent);
 
     const pasteContext = {
-        element,
+      element,
       point: {x:0, y:0}
     };
 
@@ -101,26 +117,100 @@ export default function ResourceUpdater(eventBus, modeling, elementFactory, elem
   ], ifNewResourceActivity(createChildren));
 
 
-  /// Disable modeling within resource activities
+  eventBus.on('moddleCopy.canCopyProperty', function(context) {
+    var property = context.property;
+    if (is(property, 'bpmn:ExtensionElements')) {
+      // create a copy of the extension elements in which all ids are replaced
+      let copiedProperty = modeler.get('moddle').create('bpmn:ExtensionElements');
+      copiedProperty.values = [...property.values];
+      replaceIds(copiedProperty.values);
 
-  let modelingDisabled = false;
-
-  eventBus.on("root.set", function(event) {
-    const bo = event.element.businessObject;
-    modelingDisabled = ( bo && ( bo.type == 'Resource' || bo.type == 'Request' || bo.type == 'Release' ) );
+      return copiedProperty;
+    }
   });
 
-  /// From:  bpmn-js-token-simulation/lib/features/disable-modeling/DisableModeling.js
-  function intercept(obj, fnName, cb) {
-    const fn = obj[fnName];
-    obj[fnName] = function() {
-      return cb.call(this, fn, arguments);
-    };
-  }
+  let preventEditing = false;
+  if ( preventEditing ) {
+    /// Disable modeling within resource activities
+    let modelingDisabled = false;
 
-  function ignoreIfModelingDisabled(obj, fnName) {
-    intercept(obj, fnName, function(fn, args) {
-      if (modelingDisabled) {
+    eventBus.on("root.set", function(event) {
+      const bo = event.element.businessObject;
+      modelingDisabled = ( bo && ( bo.type == 'Resource' || bo.type == 'Request' || bo.type == 'Release' ) );
+    });
+
+    /// From:  bpmn-js-token-simulation/lib/features/disable-modeling/DisableModeling.js
+    function intercept(obj, fnName, cb) {
+      const fn = obj[fnName];
+      obj[fnName] = function() {
+        return cb.call(this, fn, arguments);
+      };
+    }
+
+    function ignoreIfModelingDisabled(obj, fnName) {
+      intercept(obj, fnName, function(fn, args) {
+        if (modelingDisabled) {
+          return;
+        }
+
+        return fn.apply(this, args);
+      });
+    }
+
+    function throwIfModelingDisabled(obj, fnName) {
+      intercept(obj, fnName, function(fn, args) {
+        if (modelingDisabled) {
+          throw new Error('model is read-only');
+        }
+
+        return fn.apply(this, args);
+      });
+    }
+
+    ignoreIfModelingDisabled(contextPad, 'open');
+    ignoreIfModelingDisabled(dragging, 'init');
+    ignoreIfModelingDisabled(directEditing, 'activate');
+    ignoreIfModelingDisabled(dragging, 'init');
+    ignoreIfModelingDisabled(directEditing, 'activate');
+
+    throwIfModelingDisabled(modeling, 'moveShape');
+    throwIfModelingDisabled(modeling, 'updateAttachment');
+    throwIfModelingDisabled(modeling, 'moveElements');
+    throwIfModelingDisabled(modeling, 'moveConnection');
+    throwIfModelingDisabled(modeling, 'layoutConnection');
+    throwIfModelingDisabled(modeling, 'createConnection');
+    throwIfModelingDisabled(modeling, 'createShape');
+    throwIfModelingDisabled(modeling, 'createLabel');
+    throwIfModelingDisabled(modeling, 'appendShape');
+    throwIfModelingDisabled(modeling, 'removeElements');
+    throwIfModelingDisabled(modeling, 'distributeElements');
+    throwIfModelingDisabled(modeling, 'removeShape');
+    throwIfModelingDisabled(modeling, 'removeConnection');
+    throwIfModelingDisabled(modeling, 'replaceShape');
+    throwIfModelingDisabled(modeling, 'pasteElements');
+    throwIfModelingDisabled(modeling, 'alignElements');
+    throwIfModelingDisabled(modeling, 'resizeShape');
+    throwIfModelingDisabled(modeling, 'createSpace');
+    throwIfModelingDisabled(modeling, 'updateWaypoints');
+    throwIfModelingDisabled(modeling, 'reconnectStart');
+    throwIfModelingDisabled(modeling, 'reconnectEnd');
+
+    intercept(editorActions, 'trigger', function(fn, args) {
+      const action = args[0];
+
+      if (modelingDisabled && isAnyAction([
+        'undo',
+        'redo',
+        'copy',
+        'paste',
+        'removeSelection',
+        'spaceTool',
+        'lassoTool',
+        'globalConnectTool',
+        'distributeElements',
+        'alignElements',
+        'directEditing',
+      ], action)) {
         return;
       }
 
@@ -128,78 +218,13 @@ export default function ResourceUpdater(eventBus, modeling, elementFactory, elem
     });
   }
 
-  function throwIfModelingDisabled(obj, fnName) {
-    intercept(obj, fnName, function(fn, args) {
-      if (modelingDisabled) {
-        throw new Error('model is read-only');
-      }
-
-      return fn.apply(this, args);
-    });
-  }
-
-  ignoreIfModelingDisabled(contextPad, 'open');
-
-  ignoreIfModelingDisabled(dragging, 'init');
-
-  ignoreIfModelingDisabled(directEditing, 'activate');
-
-  ignoreIfModelingDisabled(dragging, 'init');
-
-  ignoreIfModelingDisabled(directEditing, 'activate');
-
-  throwIfModelingDisabled(modeling, 'moveShape');
-  throwIfModelingDisabled(modeling, 'updateAttachment');
-  throwIfModelingDisabled(modeling, 'moveElements');
-  throwIfModelingDisabled(modeling, 'moveConnection');
-  throwIfModelingDisabled(modeling, 'layoutConnection');
-  throwIfModelingDisabled(modeling, 'createConnection');
-  throwIfModelingDisabled(modeling, 'createShape');
-  throwIfModelingDisabled(modeling, 'createLabel');
-  throwIfModelingDisabled(modeling, 'appendShape');
-  throwIfModelingDisabled(modeling, 'removeElements');
-  throwIfModelingDisabled(modeling, 'distributeElements');
-  throwIfModelingDisabled(modeling, 'removeShape');
-  throwIfModelingDisabled(modeling, 'removeConnection');
-  throwIfModelingDisabled(modeling, 'replaceShape');
-  throwIfModelingDisabled(modeling, 'pasteElements');
-  throwIfModelingDisabled(modeling, 'alignElements');
-  throwIfModelingDisabled(modeling, 'resizeShape');
-  throwIfModelingDisabled(modeling, 'createSpace');
-  throwIfModelingDisabled(modeling, 'updateWaypoints');
-  throwIfModelingDisabled(modeling, 'reconnectStart');
-  throwIfModelingDisabled(modeling, 'reconnectEnd');
-
-  intercept(editorActions, 'trigger', function(fn, args) {
-    const action = args[0];
-
-    if (modelingDisabled && isAnyAction([
-      'undo',
-      'redo',
-      'copy',
-      'paste',
-      'removeSelection',
-      'spaceTool',
-      'lassoTool',
-      'globalConnectTool',
-      'distributeElements',
-      'alignElements',
-      'directEditing',
-    ], action)) {
-      return;
-    }
-
-    return fn.apply(this, args);
-  });
-
   // helpers //////////
 
   function isAnyAction(actions, action) {
     return actions.indexOf(action) > -1;
   }
-
 }
 
 inherits(ResourceUpdater, CommandInterceptor);
 
-ResourceUpdater.$inject = [ 'eventBus', 'modeling', 'elementFactory', 'elementRegistry', 'editorActions',  'contextPad', 'dragging', 'directEditing' ];
+ResourceUpdater.$inject = [ 'eventBus', 'modeling', /*'elementFactory',*/ 'elementRegistry', 'editorActions',  'contextPad', 'dragging', 'directEditing' ];
