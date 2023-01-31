8b8791786d827b44237e49a6172bac0d978796da
Asvin Goel <goel@telematique.eu>
2023-01-31 11:41:26 +0100

    Bugfix for labels

--- src/modules/resource/ResourceLabelProvider.js
+++ src/modules/resource/ResourceLabelProvider.js
@@ -12,7 +12,12 @@ import {
  */
 export default function ResourceLabelProvider(eventBus, modeling, textRenderer, elementRegistry) {
 
-  eventBus.on("commandStack.shape.create.preExecute", function (event) {
+  /********************************************************
+  * When copying a custom element with external label,
+  * the plane containing the label is created twice.
+  * This can be prevented by the following event handlers.
+  ********************************************************/
+  eventBus.on('commandStack.shape.create.preExecute', function (event) {
     var context = event.context,
         element = context.shape;
     if ( is(element, 'bpmn:Activity') && 
@@ -37,14 +42,14 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
            || element.oldBusinessObject.type == 'Release' 
          )
        ) {
-      // remove and re-create external label
+      // remove (defunct) label and re-create external label
       const businessObject = element.oldBusinessObject;
       modeling.removeElements([ element ]);
 
       element = elementRegistry.get(businessObject.id);
       element.label = modeling.createLabel(element, 
         { x: element.x + element.width/2, 
-          y: element.y + element.height + 16
+          y: element.y + element.height + 16,
         }, {
           id: element.businessObject.id + '_label',
           businessObject: element.businessObject
@@ -54,7 +59,16 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
         modeling.updateLabel(element.label, element.name);
       }
     }
-    else if ( is(element, 'bpmn:Activity') && 
+  });
+
+  /********************************************************
+  * When creating a custom element, an external label
+  * is created by the following event handler.
+  ********************************************************/
+  eventBus.on('commandStack.shape.create.postExecute', function(event) {
+    var context = event.context,
+        element = context.shape;
+    if ( is(element, 'bpmn:Activity') && 
          ( element.businessObject.type == 'Resource' 
            || element.businessObject.type == 'Request' 
            || element.businessObject.type == 'Release' 
@@ -63,7 +77,7 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
       // create external label
       modeling.createLabel(element, 
         { x: element.x + element.width/2, 
-          y: element.y + element.height + 16
+          y: element.y + element.height + 16,
         }, {
           id: element.businessObject.id + '_label',
           businessObject: element.businessObject
@@ -72,6 +86,11 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
     }
   });
 
+  /********************************************************
+  * When deleting the external label of a custom element, 
+  * the name of the element is deleted by the following 
+  * event handler.
+  ********************************************************/
   eventBus.on('commandStack.shape.delete.postExecute', function(event) {
     var context = event.context,
         element = context.shape;
@@ -85,7 +104,13 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
     }
   });
 
-  eventBus.on('element.changed', function(event) {
+  /********************************************************
+  * When changing the name of a custom element without  
+  * label, an external label is created by the following 
+  * event handler.
+  ********************************************************/
+
+  eventBus.on('element.changed', 99999, function(event) {
     var element = event.element;
     if ( is(element, 'bpmn:Activity') && 
          ( element.businessObject.type == 'Resource' 
@@ -101,15 +126,21 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
       bounds.x = element.x + element.width/2 - bounds.width/2;
       bounds.y = element.y + element.height + 16 - bounds.height/2;
 
-      element.di.label = { bounds };
-      element.label = modeling.createLabel(element, bounds, {
+      if ( element.di && element.di.label ) delete element.di.label;
+      modeling.createLabel(element, bounds, {
           id: element.businessObject.id + '_label',
           businessObject: element.businessObject
         }
       );
+      return false;
     }
   });
-  
+
+  /********************************************************
+  * When double clicking on a custom element without  
+  * label, an external label is created by the following 
+  * event handler.
+  ********************************************************/
   eventBus.on('element.dblclick', 99999, function(event) {
     var element = event.element;
     if ( is(element, 'bpmn:Activity') && 
@@ -121,23 +152,27 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
 
       // create external label
       var bounds = { width: 90, height: 14 };
+
       if ( element.businessObject.name ) {
         bounds = textRenderer.getExternalLabelBounds({}, element.businessObject.name);
       }
       bounds.x = element.x + element.width/2 - bounds.width/2;
       bounds.y = element.y + element.height + 16 - bounds.height/2;
 
-      element.di.label = { bounds };
-      element.label = modeling.createLabel(element, bounds, {
+      if ( element.di && element.di.label ) delete element.di.label;
+      modeling.createLabel(element, bounds, {
           id: element.businessObject.id + '_label',
           businessObject: element.businessObject
         }
       );
-
-      return false;
     }
   });
 
+  /********************************************************
+  * When loading a diagram containing custom elements with  
+  * label, the external label is created and linked to the
+  * existing di by the following  event handler.
+  ********************************************************/
   eventBus.on('shape.added', function(event) {
     var element = event.element;
     if ( is(element, 'bpmn:Activity') && 
@@ -152,6 +187,7 @@ export default function ResourceLabelProvider(eventBus, modeling, textRenderer, 
       modeling.createLabel(element, element.di.label.bounds, {
           id: element.businessObject.id + '_label',
           businessObject: element.businessObject,
+          di: element.di
         }
       );
     }
