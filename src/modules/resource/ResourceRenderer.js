27f82344dc356ff09382fe20d670e0b05e9e0d2c
Asvin Goel <goel@telematique.eu>
2023-02-21 12:22:57 +0100

    Cosmetics

--- src/modules/resource/ResourceRenderer.js
+++ src/modules/resource/ResourceRenderer.js
@@ -90,22 +90,21 @@ ResourceRenderer.prototype.drawShape = function(parentNode, element) {
   var boundary = drawRect(0, width, height, 0);
   var border = (type == 'Release') ? 4 : 2;
   if ( type == 'Resource') {
-    var rect = drawRect(0.45*height, width-0.9*height, height, 0);
-    svgAppend(parentNode, rect);
-    var halfcircle = drawShape('path', { d: "M" + 0.45*height + ",0  a1,1 0 0,0 0," + height, stroke: 'black', strokeWidth: 2, fill: 'white' } );
+    let offset=2;
+    var halfcircle = drawShape('path', { d: "M" + (0.45*height + offset) + ",0  a1,1 0 0,0 0," + height, stroke: 'black', strokeWidth: 2, fill: 'white' } );
     svgAppend(parentNode, halfcircle);
-    halfcircle = drawShape('path', { d: "M" + (width-0.45*height) + ",0  a1,1 0 0,1 0," + height, stroke: 'black', strokeWidth: 2, fill: 'white' } );
+    halfcircle = drawShape('path', { d: "M" + (width-0.45*height - offset) + ",0  a1,1 0 0,1 0," + height, stroke: 'black', strokeWidth: 2, fill: 'white' } );
     svgAppend(parentNode, halfcircle);
     var line =  drawShape('line', { x1: 0.45*height, y1: 0, x2: width-0.45*height, y2: 0, stroke: 'black', strokeWidth: 2, fill: 'white' } );
     svgAppend(parentNode, line);
     line =  drawShape('line', { x1: 0.45*height, y1: height, x2: width-0.45*height, y2: height, stroke: 'black', strokeWidth: 2, fill: 'white' } );
     svgAppend(parentNode, line);
  
-    var circle = drawCircle(0.2*height, width, height);
+    var circle = drawCircle(0.2*height+1, width, height);
     svgAppend(parentNode, circle);
     circle = drawCircle(height, width, height);
     svgAppend(parentNode, circle);
-    circle = drawCircle(1.8*height, width, height);
+    circle = drawCircle(1.8*height-1, width, height);
     svgAppend(parentNode, circle);
   }
   else {
