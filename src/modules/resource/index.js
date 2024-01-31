ae1d324f8729af7afdb39327d25afd666bee8f96
Asvin Goel <goel@telematique.eu>
2024-01-31 17:53:08 +0100

    Remove decorators

--- src/modules/resource/index.js
+++ src/modules/resource/index.js
@@ -7,12 +7,9 @@ import RequestFactory from './RequestFactory';
 import ReleaseFactory from './ReleaseFactory';
 import ResourceFactory from './ResourceFactory';
 import PreventEditing from './PreventEditing';
-import SequencerDecorator from './SequencerDecorator';
-import JobDecorator from './JobDecorator';
-import JobPopupMenu from './JobPopupMenu';
 
 export default {
-  __init__: [ 'resourceRenderer', 'resourceLabelProvider', 'resourceContextPad', 'resourcePalette', 'resourceElementsPopupMenu', 'requestFactory', 'releaseFactory', 'resourceFactory', 'preventEditing', 'sequencerDecorator', 'jobDecorator', 'jobPopupMenu' ],
+  __init__: [ 'resourceRenderer', 'resourceLabelProvider', 'resourceContextPad', 'resourcePalette', 'resourceElementsPopupMenu', 'requestFactory', 'releaseFactory', 'resourceFactory', 'preventEditing' ],
   resourceRenderer: [ 'type', ResourceRenderer ],
   resourceLabelProvider: [ 'type', ResourceLabelProvider ],
   resourceContextPad: [ 'type', ResourceContextPad ],
@@ -21,8 +18,5 @@ export default {
   requestFactory: [ 'type', RequestFactory ],
   releaseFactory: [ 'type', ReleaseFactory ],
   resourceFactory: [ 'type', ResourceFactory ],
-  preventEditing: [ 'type', PreventEditing ],
-  sequencerDecorator: [ 'type', SequencerDecorator ],
-  jobDecorator: [ 'type', JobDecorator ],
-  jobPopupMenu: [ 'type', JobPopupMenu ]
+  preventEditing: [ 'type', PreventEditing ]
 };
