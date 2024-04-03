67116459d715912a437818fc947b1884b679f258
Asvin Goel <goel@telematique.eu>
2024-04-03 13:23:17 +0200

    Move instance attribute to data object

--- src/modules/bpmnos/properties/ExecutableProps.js
+++ src/modules/bpmnos/properties/ExecutableProps.js
@@ -109,22 +109,45 @@ function hasProcessRef(element) {
   return is(element, 'bpmn:Participant') && element.businessObject.get('processRef');
 }
 
+function hasInstanceAttribute(element) {
+  let dataObjects = element.businessObject.flowElements.filter((element) => {
+    return element.$type === 'bpmn:DataObject' && element.extensionElements && element.extensionElements.values.length;
+  });
+  for ( let dataObject of dataObjects ) {
+    for ( let attribute of dataObject.extensionElements.values[0].get('attribute') ) {
+      if ( attribute.id == "Instance" ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function ensureDefaultAttributes(element,bpmnFactory,commandStack) {
+  if ( !hasInstanceAttribute(element) ) {
+    var dataObject = bpmnFactory.create('bpmn:DataObject');
+    const attributes = ensureCustomItem(bpmnFactory, commandStack, dataObject, 'bpmnos:Attributes');
+    const attribute = createElement('bpmnos:Attribute', { id: 'Instance' , name: 'instance', type: 'string' }, attributes, bpmnFactory);
+    attributes.get('attribute').push(attribute);
+    element.businessObject.flowElements.push(dataObject);
+  }
+
   const status = ensureCustomItem(bpmnFactory, commandStack, element, 'bpmnos:Status'); 
   let attributes = status.get('attributes') ? status.attributes[0] : undefined;
   if ( !attributes ) {
     // create 'bpmnos:Attributes'
     attributes = createElement('bpmnos:Attributes', {}, parent, bpmnFactory);
     commandStack.execute('element.updateModdleProperties', {
-        element,
-        moddleElement: status,
-        properties: {
+      element,
+      moddleElement: status,
+      properties: {
         attributes: [ attributes ]
       }
     });
   }
 
   let attributeList = attributes.get('attribute') || [];
+/*
   if ( !attributeList.find(attribute => attribute.name == "instance") ) {
       const attribute = createElement('bpmnos:Attribute', { id: 'Instance' , name: 'instance', type: 'string' }, attributes, bpmnFactory);
       commandStack.execute('element.updateModdleProperties', {
@@ -135,14 +158,15 @@ function ensureDefaultAttributes(element,bpmnFactory,commandStack) {
         }
       });
   }
+*/
   if ( !attributeList.find(attribute => attribute.name == "timestamp") ) {
-      const attribute = createElement('bpmnos:Attribute', { id: 'Timestamp' , name: 'timestamp', type: 'decimal', value: '0' }, attributes, bpmnFactory);
-      commandStack.execute('element.updateModdleProperties', {
-        element,
-        moddleElement: attributes,
-        properties: {
-          attribute: [ ...attributes.get('attribute'), attribute ]
-        }
-      });
+    const attribute = createElement('bpmnos:Attribute', { id: 'Timestamp' , name: 'timestamp', type: 'decimal', value: '0' }, attributes, bpmnFactory);
+    commandStack.execute('element.updateModdleProperties', {
+      element,
+      moddleElement: attributes,
+      properties: {
+        attribute: [ ...attributes.get('attribute'), attribute ]
+      }
+    });
   }
 }
